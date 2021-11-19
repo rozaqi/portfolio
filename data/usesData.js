5aa951a5f77f8ab37c78d0760897cd37a156b06a
Abdul Rozaqi W <61746302+rozaqiw@users.noreply.github.com>
2021-11-19 13:50:43 +0700

    Update usesData.js

--- data/usesData.js
+++ data/usesData.js
@@ -1,48 +1,15 @@
 const usesData = [
   {
-    title: 'Docker',
-    description: `Docker is a set of platform as a service products that use OS-level virtualization to deliver software in packages called containers.`,
-    href: '#',
+    title: 'Cyber Security',
   },
   {
-    title: 'Kubernetes',
-    description: 'Kubernetes is an open-source container-orchestration system for automating computer application deployment, scaling, and management.',
-    href: '#',
+    title: 'Cloud Computing',
   },
   {
-    title: 'Openstack',
-    description: 'OpenStack is a free, open standard cloud computing platform. It is mostly deployed as infrastructure-as-a-service in both public and private clouds where virtual servers and other resources are made available to users.',
-    href: '#',
+    title: 'Computer Networking',
   },
-  {
-    title: 'Ansible',
-    description: 'Ansible is an open-source software provisioning, configuration management, and application-deployment tool enabling infrastructure as code. It runs on many Unix-like systems, and can configure both Unix-like systems as well as Microsoft Windows.',
-    href: '#',
-  },
-  {
-    title: 'CEPH',
-    description: 'Ceph is an open-source software storage platform, implements object storage on a single distributed computer cluster, and provides 3-in-1 interfaces for object-, block- and file-level storage.',
-    href: '#',
-  },
-  {
-    title: 'Linux',
-    description: 'Linux is a family of open-source Unix-like operating systems based on the Linux kernel, an operating system kernel first released on September 17, 1991, by Linus Torvalds.',
-    href: '#',
-  },
-  {
-    title: 'Python',
-    description: 'Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant indentation.',
-    href: '#',
-  },
-  {
-    title: 'Bourne Again Shell',
-    description: 'Bash is a Unix shell and command language written by Brian Fox for the GNU Project as a free software replacement for the Bourne shell.',
-    href: '#',
-  },
-  {
-    title: 'PHP',
-    description: 'PHP is a general-purpose scripting language geared towards web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.',
-    href: '#',
+    {
+    title: 'Linux System Administrator',
   },
 ]
 
