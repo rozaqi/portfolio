38601b16009406856d99fc5316372f56492c5ebd
Abdul Rozaqi W <61746302+rozaqiw@users.noreply.github.com>
2021-11-19 13:09:49 +0700

    Update saas-kit.js

--- pages/saas-kit.js
+++ pages/saas-kit.js
@@ -5,18 +5,53 @@ export default function SaasKit() {
   return (
     <>
       <PageSEO
-        title={`SaaS Kit - ${siteMetadata.author}`}
-        description={`Saas Kit - ${siteMetadata.author} - Helping to grow digital product agencies and indie developers.`}
+        title={`Achievements - ${siteMetadata.author}`}
+        description={`Achievements - ${siteMetadata.author} - my experience and achievements`}
       />
-      <div className="divide-y">
-        <div className="mt-24 text-center">
+        <div className="">
+        <h1 className="text-center font-extrabold text-2xl leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-4xl md:leading-14">Achievements :</h1><br />
           <h1>
-            This page is currently a draft. Look back later to see what's changed.{' '}
-            <span role="img" aria-label="roadwork sign">
-              🚧
-            </span>
-          </h1>
-        </div>
+<ul className="list-disc flex justify-center">
+  <li>Finalist Olimpiade Cyber Security dan IoT ARA ITS 2021</li>
+  
+</ul>
+<h2 className="text-center">March 2021</h2>
+          </h1><br />
+          <h1>
+<ul className="list-disc flex justify-center">
+  <li>5th place Capture The Flag at KKS TNI-AD</li>
+  
+</ul>
+<h2 className="text-center">November 2020</h2>
+          </h1><br />
+          <h1>
+<ul className="list-disc flex justify-center">
+  <li>Finalist Capture The Flag competition at Hology</li>
+  
+</ul>
+<h2 className="text-center">November 2020</h2>
+          </h1><br />
+          <h1>
+<ul className="list-disc flex justify-center">
+  <li>4th National Cyber Security LKS</li>
+  
+</ul>
+<h2 className="text-center">October 2020</h2>
+          </h1><br />
+    <h1>
+<ul className="list-disc flex justify-center">
+  <li>Finalist Capture The Flag competition at HackToday</li>
+  
+</ul>
+<h2 className="text-center">August 2020</h2>
+          </h1><br />
+           <h1>
+<ul className="list-disc flex justify-center">
+  <li>4th place Capture The Flag competition at iCyption</li>
+  
+</ul>
+<h2 className="text-center">July 2020</h2>
+          </h1><br />
       </div>
     </>
   )
